d79966d5313f7f719c55c7dfbbe3649f1b1c3b7b
serch <serchrod96@gmail.com>
2019-01-24 10:54:47 -0600

    adding feature to postgres Connection script

--- Database/PostgresConnection.py
+++ Database/PostgresConnection.py
@@ -6,9 +6,6 @@ import pandas
 import psycopg2
 
 
-
-#connection class for postgres or amazon redshift
-
 class PostgresConnection():
 
     def __init__(self):
@@ -16,7 +13,7 @@ class PostgresConnection():
         self.cursor=None
         self.host='localhost'
         self.user='postgres'
-        self.database=''
+        self.database='postgres'
         self.isredshift=False
         self.password=''
         self.psypg2_url=''
@@ -32,7 +29,7 @@ class PostgresConnection():
                 self.conn=psycopg2.connect("dbname='{db}' user='{usr}' host='{ip}' password='{passwd}'"
                 .format(db=self.database,usr=self.user,ip=self.host,passwd=self.password))
                 self.cursor = self.conn.cursor()
-
+                
             except(psycopg2.OperationalError):
                 print("could not connect")
                 sys.exit(0)
@@ -44,17 +41,28 @@ class PostgresConnection():
                 print("could not connect")
                 sys.exit(0)
 
+    #return rows affected
+    def executeCommand(self,query):
+        if self.cursor != None:
+            self.cursor.execute(query)
+            rows_deleted = self.cursor.rowcount
+            self.conn.commit()
+            return rows_deleted
+        else:
+            print('Connection not established')
+            return []
+
+    #return results from a query
     def executeQuery(self,query):
         if self.cursor != None:
             self.cursor.execute(query)
             results=self.cursor.fetchall()
-            
-
             return results
         else:
             print('Connection not established')
             return []
 
+    #transform a pandas object 
     def queryToPandas(self,query):
         results=self.executeQuery(query)
         column_names = [desc[0] for desc in self.cursor.description]
@@ -65,6 +73,12 @@ class PostgresConnection():
             print('failed on create dataframe operation')
             sys.exit()
 
+    #close a connection
     def closeConnection(self):
+        self.cursor.close()
         self.conn.close()
+
+     #destructor
+     def __del__(self): 
+        self.closeConnection()
     
